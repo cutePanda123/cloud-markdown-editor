32475306a8bc0973d00321498501177e8c1720e6
Peter Li <peterli1234@126.com>
2021-01-21 07:40:51 -0800

    included bootstrap and initiated a grid system for the main view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      <div className="row">
+        <div className="col-3 bg-danger left-panel">
+          <h1>Left panel!!!</h1>
+        </div>
+        <div className="col-9 bg-primary right-panel">
+          <h1>Right panel!!!</h1>
+        </div>
+      </div>
     </div>
   );
 }
