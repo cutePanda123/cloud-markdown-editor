1c5d8261ceff9b10e1f2d8dbb9f00b773c0b2bca
Peter Li <peterli1234@126.com>
2021-01-22 08:58:57 -0800

    implemented file search component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FileSearch from './componments/FileSearch';
 
 function App() {
   return (
     <div className="App container-fluid">
       <div className="row">
         <div className="col-3 bg-danger left-panel">
-          <h1>Left panel!!!</h1>
+          <FileSearch 
+            title={'My files'}
+            onFileSearch={null}
+          />
         </div>
         <div className="col-9 bg-primary right-panel">
           <h1>Right panel!!!</h1>
